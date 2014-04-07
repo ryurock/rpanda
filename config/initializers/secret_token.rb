55b23544b86f8d82b204361419ec7660e8f73b00
vagrant <ryusukekimura3@gmail.com>
2014-04-07 12:00:33 +0900

    new porject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rpanda::Application.config.secret_key_base = '7212bcbeec6c6a7fde929ed6a6e87e149b185e205571006c573d2bb4480ab0a21ca0e13fc713c8d0a82583ee3c07c494435ee3f09d4cbe12b2d69816e672ea1c'
